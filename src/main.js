1a8bdfcd58a5d235c2842585aa0c26ade5352a3c
jbnilles <jbnilles24@gmail.com>
2020-09-08 15:47:44 -0700

    add everything
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,7 +3,66 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+function getElements(response) {
+$('#question').html(response.results[0].question);
+
+$('#answer').html(response.results[0].correct_answer);
+}
+function displayCategories() {
+  let request = new XMLHttpRequest();
+  const url =  "https://opentdb.com/api_category.php"
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      let html = '';
+      for (let i=0; i<response.trivia_categories.length; i++) {
+        html = '';
+        html +=  `<option value="${response.trivia_categories[i].id}">`;
+        html += `${response.trivia_categories[i].name}`;
+        html += `</option>`;
+        $('#trivia-questions').append(html);
+      }
+    }
+  }
+  request.open("GET", url, true);
+  request.send();
+}
+
+function getQuestion(categoryId) {
+  $('#answer').hide();
+  let request = new XMLHttpRequest();
+  const url =  `https://opentdb.com/api.php?amount=1&category=${categoryId}`
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  }
+  request.open("GET", url, true);
+  request.send();
+}
 
 $(document).ready(function() {
-  
+  let categoryId = 9;
+  getQuestion(categoryId);
+  displayCategories();
+$('#next-button').click(function () {
+  getQuestion(categoryId);
 });
+
+$('#answer-button').click(function () {
+  $('#answer').show();
+});
+
+$('#trivia-questions').change(function () {
+//console.log($('#trivia-questions option:selected').val());
+categoryId = $('#trivia-questions option:selected').val();
+getQuestion(categoryId);
+});
+
+
+});
+
+//http://jservice.io/api/random
+//https://opentdb.com/api.php?amount=1&category=17
